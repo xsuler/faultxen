09249077bba790760dece6d7a7adee10975fed98
xsuler <aeonsule@gmail.comm>
2020-03-21 15:46:46 +0000

    fix

--- xen/include/compat/elfnote.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COMPAT_ELFNOTE_H
-#define _COMPAT_ELFNOTE_H
-#include <xen/compat.h>
-#include <public/elfnote.h>
-#pragma pack(push, 4)
-
-#pragma pack(pop)
-#endif /* _COMPAT_ELFNOTE_H */
